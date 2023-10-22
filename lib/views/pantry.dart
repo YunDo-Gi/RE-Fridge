5cdb8d2d8095e8c9046678e73498fcba12266208
Yuncheol Kwak <lvbhkwak@gmail.com>
2023-10-22 22:28:27 +0900

    FEAT: Add TabView

--- lib/views/pantry.dart
+++ lib/views/pantry.dart
@@ -3,39 +3,173 @@ import 'package:get/get.dart';
 
 import 'package:re_fridge/controllers/pantry_controller.dart';
 
+const List<String> categorys = <String>[
+  'All',
+  'Vegetables',
+  'Meat',
+  'Fish / Seafood',
+  'Dairy / Egg',
+];
+
 class Pantry extends StatelessWidget {
   Pantry({Key? key}) : super(key: key);
   final pantryController = Get.put(PantryController());
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
+    int tabsCount = categorys.length;
+
+    return DefaultTabController(
+        length: tabsCount,
+        child: Scaffold(
+          appBar: AppBar(
+            centerTitle: false,
+            notificationPredicate: (ScrollNotification notification) {
+              return notification.depth == 1;
+            },
+            scrolledUnderElevation: 0.0,
+            title: Text('Pantry'),
+            bottom: TabBar(
+              tabs: <Widget>[
+                Tab(
+                  text: categorys[0],
+                ),
+                Tab(
+                  text: categorys[1],
+                ),
+                Tab(
+                  text: categorys[2],
+                ),
+                Tab(
+                  text: categorys[3],
+                ),
+                Tab(
+                  text: categorys[4],
+                ),
+              ],
+            ),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 0),
             child: GetX<PantryController>(
               builder: (controller) {
-                return ListView.builder(
-                  itemCount: controller.ingredients.length,
-                  itemBuilder: (context, index) {
-                    return Card(
-                      margin: EdgeInsets.all(8.0),
-                      child: ListTile(
-                        leading: Icon(Icons.food_bank),
-                        title: Text(controller.ingredients[index].ingredientName),
-                        subtitle: Text(controller.ingredients[index].expiryDate.toString()),
-                        subtitleTextStyle: TextStyle(color: Colors.red),
-                        trailing: Text(controller.ingredients[index].quantity.toString()),
-                        contentPadding: EdgeInsets.fromLTRB(20, 0, 20, 0),
-                      ),
-                    );
-                  },
+                return TabBarView(
+                  children: <Widget>[
+                    // All
+                    ListView.builder(
+                      itemCount: controller.ingredients.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          margin: EdgeInsets.all(8.0),
+                          child: ListTile(
+                            leading: Icon(Icons.food_bank),
+                            title: Text(
+                                controller.ingredients[index].ingredientName),
+                            subtitle: Text(controller
+                                .ingredients[index].expiryDate
+                                .toString()),
+                            subtitleTextStyle: TextStyle(color: Colors.red),
+                            trailing: Text(controller
+                                .ingredients[index].quantity
+                                .toString()),
+                            contentPadding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                          ),
+                        );
+                      },
+                    ),
+                    // Vegetable
+                    ListView.builder(
+                      itemCount: controller.ingredients.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          margin: EdgeInsets.all(8.0),
+                          child: ListTile(
+                            leading: Icon(Icons.food_bank),
+                            title: Text(
+                                controller.ingredients[index].ingredientName),
+                            subtitle: Text(controller
+                                .ingredients[index].expiryDate
+                                .toString()),
+                            subtitleTextStyle: TextStyle(color: Colors.red),
+                            trailing: Text(controller
+                                .ingredients[index].quantity
+                                .toString()),
+                            contentPadding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                          ),
+                        );
+                      },
+                    ),
+                    // Meat
+                    ListView.builder(
+                      itemCount: controller.ingredients.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          margin: EdgeInsets.all(8.0),
+                          child: ListTile(
+                            leading: Icon(Icons.food_bank),
+                            title: Text(
+                                controller.ingredients[index].ingredientName),
+                            subtitle: Text(controller
+                                .ingredients[index].expiryDate
+                                .toString()),
+                            subtitleTextStyle: TextStyle(color: Colors.red),
+                            trailing: Text(controller
+                                .ingredients[index].quantity
+                                .toString()),
+                            contentPadding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                          ),
+                        );
+                      },
+                    ),
+                    // Fish / Seafood
+                    ListView.builder(
+                      itemCount: controller.ingredients.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          margin: EdgeInsets.all(8.0),
+                          child: ListTile(
+                            leading: Icon(Icons.food_bank),
+                            title: Text(
+                                controller.ingredients[index].ingredientName),
+                            subtitle: Text(controller
+                                .ingredients[index].expiryDate
+                                .toString()),
+                            subtitleTextStyle: TextStyle(color: Colors.red),
+                            trailing: Text(controller
+                                .ingredients[index].quantity
+                                .toString()),
+                            contentPadding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                          ),
+                        );
+                      },
+                    ),
+                    // Dairy / Egg
+                    ListView.builder(
+                      itemCount: controller.ingredients.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          margin: EdgeInsets.all(8.0),
+                          child: ListTile(
+                            leading: Icon(Icons.food_bank),
+                            title: Text(
+                                controller.ingredients[index].ingredientName),
+                            subtitle: Text(controller
+                                .ingredients[index].expiryDate
+                                .toString()),
+                            subtitleTextStyle: TextStyle(color: Colors.red),
+                            trailing: Text(controller
+                                .ingredients[index].quantity
+                                .toString()),
+                            contentPadding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                          ),
+                        );
+                      },
+                    )
+                  ],
                 );
               },
             ),
           ),
-        ],
-      ),
-    );
+        ));
   }
 }
