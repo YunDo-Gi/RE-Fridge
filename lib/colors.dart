7d969cf4bda36c72815f5cf3a7ce262919617953
Yuncheol Kwak <lvbhkwak@gmail.com>
2023-11-14 23:15:10 +0900

    design: Add card slidable

---
+++ lib/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+// 주색상
+const Color PRIMARY_COLOR = Color(0xFF8EB44E);
+// 글자 색상
+const Color BACKGROUND_COLOR= Color(0xFFF5ECDC);
--- lib/widgets/pantry_item.dart
+++ lib/widgets/pantry_item.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart'; 
+import 'package:get/get.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 import 'package:re_fridge/controllers/pantry_controller.dart';
+import 'package:re_fridge/colors.dart';
 
 class PantryItem extends StatefulWidget {
   final int index;
   const PantryItem({Key? key, required this.index}) : super(key: key);
-  
 
   @override
   _PantryItemState createState() => _PantryItemState();
@@ -18,40 +19,81 @@ class _PantryItemState extends State<PantryItem> {
 
   @override
   Widget build(BuildContext context) {
+    const cardRadius = 12.0;
+
     return Card(
-      margin:
-          EdgeInsets.fromLTRB(8.0, 12.0, 8.0, 0),
-      elevation: 2,
-      child: ListTile(
-        leading: Icon(
-          Icons.food_bank,
-          size: 36,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(cardRadius),
         ),
-        title: Text(
-            controller.foundIngredients[index]
-                .ingredientName,
-            style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w500,
-                color: Color.fromRGBO(
-                    54, 40, 34, 1))),
-        subtitle: controller.toDDay(controller
-            .foundIngredients[index].expiryDate),
-        subtitleTextStyle: TextStyle(
-            color: Colors.red, height: 1.5),
-        trailing: Text(
-            controller.foundIngredients[index]
-                    .quantity
-                    .toString() +
-                ' g',
-            style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w500,
-                color: Color.fromRGBO(
-                    54, 40, 34, 1))),
-        contentPadding:
-            EdgeInsets.fromLTRB(20, 0, 20, 0),
-      ),
-    );
+        elevation: 2,
+        child: Slidable(
+          key: const ValueKey(0),
+          startActionPane: ActionPane(
+            // width of action pane
+            extentRatio: 0.25,
+            // A motion is a widget used to control how the pane animates.
+            motion: const ScrollMotion(),
+
+            // A pane can dismiss the Slidable.
+            dismissible: DismissiblePane(onDismissed: () {}),
+
+            // All actions are defined in the children parameter.
+            children: [
+              // A SlidableAction can have an icon and/or a label.
+              SlidableAction(
+                autoClose: true,
+                onPressed: deleteIngredient(),
+                backgroundColor: PRIMARY_COLOR,
+                foregroundColor: Colors.white,
+                icon: Icons.shopping_cart,
+                label: 'To Cart',
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(cardRadius),
+                    bottomLeft: Radius.circular(cardRadius))
+              ),
+            ],
+          ),
+          child: ListTile(
+            leading: Icon(
+              Icons.food_bank,
+              size: 36,
+            ),
+            title: Text(controller.foundIngredients[index].ingredientName,
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                    color: Color.fromRGBO(54, 40, 34, 1))),
+            subtitle: controller
+                .toDDay(controller.foundIngredients[index].expiryDate),
+            subtitleTextStyle: TextStyle(color: Colors.red, height: 1.8),
+            trailing: Text(
+                controller.foundIngredients[index].quantity.toString() + ' g',
+                style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                    color: Color.fromRGBO(54, 40, 34, 1))),
+            contentPadding: EdgeInsets.fromLTRB(16, 8, 16, 8),
+          ),
+          endActionPane: ActionPane(
+            extentRatio: 0.25,
+            motion: const ScrollMotion(),
+            dismissible: DismissiblePane(onDismissed: () {}),
+            children: [
+              SlidableAction(
+                autoClose: true,
+                onPressed: deleteIngredient(),
+                backgroundColor: Color(0xFFFE4A49),
+                foregroundColor: Colors.white,
+                icon: Icons.delete,
+                label: 'Delete',
+                borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(cardRadius),
+                    bottomRight: Radius.circular(cardRadius))
+              ),
+            ],
+          ),
+        ));
   }
+
+  deleteIngredient() {}
 }
